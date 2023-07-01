5b2116871ea589d7bf43d090616b130c2eb03e47
3kiein <daisy.airdrop_08@icloud.com>
2023-07-01 23:47:31 +0900

    add news dummy
    Co-authored-by: JiHun <tedhun21@users.noreply.github.com>

---
+++ repository/marketItems.js
@@ -0,0 +1,13 @@
+module.exports.marketItems = [{
+    id: 1,
+    createdAt: "2023-07-01T21:46:00.000Z",
+    title: "70년대 일제 하카란다 클래식",
+    author: "nicedream",
+    content: "70년대 일본 카스가에서 제작한 탑솔리드 기타입니다. 하카란다 측후판에 바인딩도 멋집니다. 넥휨 배브름없습니다. 가격은 300000 으로 올렸지만 너트폭이 좁은 클레식기타 메이플측후판 기타 할로우바디 등과의 교환이 가능하면 더 좋겠습니다. 분당 야탑 직거래만 가능합니다. 소케포함합니다.",
+    mainCAT: "어쿠스틱/클래식기타",
+    subCAT: "클래식기타",
+    picture: "https://cdn.imweb.me/thumbnail/20190330/5c9f0a0172a3e.jpg",
+    price: 30,
+    region: '경기',
+    onsale: true
+}];
--- router/Router.js
+++ router/Router.js
@@ -1,19 +1,25 @@
 const express = require("express")
 const router = express.Router()
 
-const notices = require('../repository/notice')
+const notices = require('../repository/noticeItems')
+const news = require('../repository/newsItems')
+const markets = require('../repository/marketItems')
 
 // notice
-router.get('/notice', (req,res) => {
+router.get('/notice', (req, res) => {
     res.send(notices)
 })
 
 // market
-router.get('/market', () => {})
+router.get('/market', (req, res) => {
+    res.send(markets)
+})
 router.post('/market', () => {})
 
 // news
-router.get('/news', () => {})
+router.get('/news', (req, res) => {
+    res.send(news)
+})
 router.post('/news', () => {})
 
 module.exports = router;
