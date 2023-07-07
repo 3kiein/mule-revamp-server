98cc668efe88c35c5c7e9d202627435cebcc25ab
tedhun21 <songjihun940409@gmail.com>
2023-07-07 19:49:17 +0900

    login & send cookie

--- router/Router.js
+++ router/Router.js
@@ -27,13 +27,26 @@ router.post("/news", () => {});
 router.post("/login", (req, res) => {
   const { userId, userPassword } = req.body;
 
-  const userInfo = userData.filter(
-    (user) => user.userId === userId && user.password === userPassword
-  )[0];
-  console.log(userInfo);
-  if (!userInfo) {
-    res.status(401).send("일치하는 유저가 없습니다.");
+  const userInfo = {
+    ...userData.filter(
+      (user) => user.userId === userId && user.password === userPassword
+    )[0],
+  };
+
+  const cookiesOption = {
+    domain: "localhost",
+    path: "/",
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+  };
+
+  if (userInfo.id === undefined) {
+    res.status(401).send("Not Authorized");
   } else if (userInfo) {
+    cookiesOption.maxAge = 1000 * 60 * 30;
+    cookiesOption.expires = new Date(Date.now() + 1000 * 60 * 30);
+    res.cookie("userId", userInfo.id, cookiesOption);
     delete userInfo.password;
     res.status(200).send(userInfo);
   }
